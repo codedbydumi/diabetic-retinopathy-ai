f3dc924d22d83a11e1107f607542ea062c78bcee
codedbydumi <duminduthushan9@gmail.com>
2025-09-08 01:17:41 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -487,7 +487,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "*"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001" ,"https://diabetic-retinopathy-ai-two.vercel.app/" ,"*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
